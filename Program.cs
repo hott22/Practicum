38d271ce3bac87d58936a6ad76b31b9e418b4aa4
Bobby <mif210210@gmail.com>
2021-12-28 00:07:35 +0300

    edit program

--- Program.cs
+++ Program.cs
@@ -2,27 +2,47 @@
 //формирует массив из чисел, больших 8. Первоначальный массив можно ввести с клавиатуры, 
 //либо сгенерировать случайным образом. 
 
-int[] array = new int[10];
-void FirstArray (int[] arr)
+int[] array = new int[7];
+int[] secondArray = new int[array.Length]; 
+
+void FirstArray(int[] arr)
 {
     for (int i = 0; i < arr.Length; i++)
     {
         arr[i] = new Random().Next(3, 12);
-    
-        Console.Write($"{arr[i]} ");
     }
 }
 
-
-void SecondArray (int[] arr2)
+void PrintArray(int[] arr2)
 {
+    
     for (int i = 0; i < arr2.Length; i++)
     {
-        if (arr2[i] > 8) Console.Write($"{arr2[i]} ");
+        Console.Write($"{arr2[i]}, ");
     }
+    Console.Write("]");
 }
 
-FirstArray (array);
-Console.WriteLine();
-SecondArray (array);
+void SecondArray(int [] temp, int [] arr3) 
+{ 
+    for (int i = 0; i < temp.Length; i++) 
+    { 
+        
+        if(temp[i] > 8 )  
+        { 
+            arr3[i] = temp[i]; 
+            Console.Write(arr3[i] + ", "); 
+        } 
+        
+    } 
+} 
+
+
+FirstArray(array);
+PrintArray(array);
+Console.Write(" => ");
+Console.Write("[");
+SecondArray(array, secondArray); 
+Console.WriteLine("]");
+
 
