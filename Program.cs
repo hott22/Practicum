108e81542f366b0c6259fbb9ec4991c18d98aab9
Bobby <mif210210@gmail.com>
2021-12-27 15:20:09 +0300

    add Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Написать программу, которая из имеющегося массива целых чисел 
+//формирует массив из чисел, больших 8. Первоначальный массив можно ввести с клавиатуры, 
+//либо сгенерировать случайным образом. 
+
+int[] array = new int[10];
+void FirstArray (int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(3, 12);
+    
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+
+void SecondArray (int[] arr2)
+{
+    for (int i = 0; i < arr2.Length; i++)
+    {
+        if (arr2[i] > 8) Console.Write($"{arr2[i]} ");
+    }
+}
+
+FirstArray (array);
+Console.WriteLine();
+SecondArray (array);
+
